1be3f4fb15d5eb8dfc1301f6285bfb367daa321d
inmate4588 <96914912+inmate4588@users.noreply.github.com>
2024-08-19 20:38:33 +1000

    Update config.ts

--- app/utils/aws/config.ts
+++ app/utils/aws/config.ts
@@ -3,8 +3,8 @@ import { ResourcesConfig } from "aws-amplify";
 const amplifyConfig: ResourcesConfig = {
     Auth: {
       Cognito: {
-        userPoolClientId: '358bfgi6ffmblbitgs6a0ooi1m',
-        userPoolId: 'us-east-1_0IZsUv8HN',
+        userPoolClientId: '503oomostkuagui1pijcfj05ee',
+        userPoolId: 'ap-southeast-2_jTYJ9Ia79',
         }
       }
     }
